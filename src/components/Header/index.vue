b7c0f6349956b48afb50f3d344409d2c96c5b244
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-17 17:20:40 +0500

    feat: add Header component

---
+++ src/components/Header/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1 class="title">{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+import { data } from './state';
+
+export default {
+  data,
+};
+</script>
+
+<style scoped type="scss">
+header {
+  padding: 15px 0 25px 0;
+  margin-bottom: 30px;
+}
+
+.title {
+  text-align: center;
+  font-size: 3.5rem;
+  color: #fff;
+  text-shadow: 1px 1px 1px black;
+}
+</style>
