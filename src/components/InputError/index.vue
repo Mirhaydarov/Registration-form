2db040b00ed5504e10121793d621e8c1285604c6
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-17 17:31:47 +0500

    feat: add InputError component
    InputError take a props for init and render div with message about mistake
    if form' input have an error.

---
+++ src/components/InputError/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="form__error"
+    v-if="vIf">
+      {{ message }}.
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    vIf: {
+      type: Boolean,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.form__error {
+  color: red;
+  font-size: 1rem;
+}
+
+</style>
