88f55672b7ec27bb526f426c51023e795bbd8555
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-17 17:43:42 +0500

    feat: add Address state
    Address is one of the component to form.

---
+++ src/components/Form/Address/state.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/prefer-default-export
+export const data = () => ({
+  formLabel: {
+    index: 'Индекс',
+    region: 'Область',
+    country: 'Страна',
+    city: 'Город',
+    street: 'Улица',
+    house: 'Дом ',
+  },
+
+  index: null,
+  region: '',
+  country: '',
+  city: '',
+  street: '',
+  house: '',
+
+  placeholder: {
+    index: 101000,
+    region: 'Московская область',
+    country: 'Россия',
+    city: 'Москва',
+    street: 'Кузнецкий Мост',
+    house: '203',
+  },
+
+  required: '*Поле обязательное для заполнения.',
+  invalid: true,
+});
